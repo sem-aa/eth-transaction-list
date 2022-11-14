501d69ac8d7e03816e48af58837585cf79983935
sem-aa <66129330+sem-aa@users.noreply.github.com>
2022-11-14 10:26:17 +0200

    build

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path')
 const app = express();
 const server = require("http").createServer(app);
 const routes = require("./routes/routes");
 const db = require("./model/db");
-const path = require('path')
 
 const PORT = process.env.PORT || 4444;
 
@@ -13,23 +13,9 @@ app.use(cors({ credentials: true, origin: "*" }));
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use("/api", routes);
 
-app.use((_, res, __) => {
-  res.status(404).json({
-    status: "error",
-    code: 404,
-    message: "Wrong path",
-    data: "Not found",
-  });
-});
-
-app.use((err, _, res, __) => {
-  console.log(err.stack);
-  res.status(500).json({
-    status: "fail",
-    code: 500,
-    message: err.message,
-    data: "Internal Server Error",
-  });
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  res.status(status).json({ message: err.message });
 });
 
 app.use("/", express.static(path.join(__dirname, "client", "build")));
